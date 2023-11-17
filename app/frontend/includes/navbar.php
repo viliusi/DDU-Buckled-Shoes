4a409bdaa5e1b14a40a1cf7e3ea401fa5eec4400
Tobias <110847292+tottomoup@users.noreply.github.com>
2023-11-17 12:53:08 +0100

    Last update for today

--- app/frontend/includes/navbar.php
+++ app/frontend/includes/navbar.php
@@ -7,10 +7,10 @@
     <ul class="navbar-nav">
       <?php if ($user->isLoggedIn()) : ?>
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Home</a>
+          <a class="nav-link" href="index.php">Shoes</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="forum.php">Forum</a>
+          <a class="nav-link" href="index.php">Accessories</a>
         </li>
       <?php endif; ?>
     </ul>
@@ -19,16 +19,26 @@
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="profile.php">
-            <span class="glyphicon glyphicon-user"></span> Profile
+            <span class="glyphicon glyphicon-user"></span> Account
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="logout.php">
-            <span class="glyphicon glyphicon-log-out"></span> Logout
+          <a class="nav-link" href="index.php">
+            <span class="glyphicon glyphicon-log-out"></span> 🛒
           </a>
         </li>
       </ul>
     <?php else : ?>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Shoes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Accessories</a>
+          </li>
+          </li>
+        </ul>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <a class="nav-link" href="register.php">
