16e838d3a926e2ff1f482b95ce1f7dc49e03736e
Tobias <110847292+tottomoup@users.noreply.github.com>
2023-11-20 15:10:37 +0100

    Admin wokring

--- app/frontend/includes/footer.php
+++ app/frontend/includes/footer.php
@@ -1,5 +1,7 @@
 <div class="jumbotron text-center" style="margin-bottom:0">
-  <?php if ($user->isAdmin()) : ?>
+  <?php 
+  if ($user->isAdmin()) : 
+  ?>
       <p><a href="management-hub.php">Management Page</a></p>
   <?php endif; ?>
   <p>© 2023. UWU games (I/S)</p>
