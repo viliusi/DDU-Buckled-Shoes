02920ff4a58a0cb04000a40cf84d8e9ce255b42b
Tobias <tobiasflament2@gmail.com>
2023-11-28 13:51:32 +0100

    Starting on product sites

--- app/frontend/pages/shoes.php
+++ app/frontend/pages/shoes.php
@@ -4,16 +4,19 @@
   </div>
   <div class="row">
     <div class="col-sm-4">
+    <a href="classic.php">
       <h3>Classic</h3>
       <p>Shoe 1 image</p>
       <p>Shoe 1 price</p>
     </div>
     <div class="col-sm-4">
+    <a href="classic.php">
       <h3>Wheele</h3>
       <p>Shoe 2 image</p>
       <p>Shoe 2 price</p>
     </div>
     <div class="col-sm-4">
+    <a href="classic.php">
       <h3>MOON-E</h3>
       <p>Shoe 3 image</p>
       <p>Shoe 3 price</p>
@@ -21,11 +24,13 @@
   </div>
   <div class="row">
     <div class="col-sm-4">
+    <a href="classic.php">
       <h3>WhimsiQuirk</h3>
       <p>Shoe 4 image</p>
       <p>Shoe 4 price</p>
     </div>
     <div class="col-sm-4">
+    <a href="classic.php">
       <h3>😍Stompers</h3>
       <p>Shoe 5 image</p>
       <p>Shoe 5 price</p>
