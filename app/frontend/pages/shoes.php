5bf0781ead98d4520ebf601d5666459b3d0e44c5
Tobias <110847292+tottomoup@users.noreply.github.com>
2023-12-12 14:38:08 +0100

    Show products from their id done

--- app/frontend/pages/shoes.php
+++ app/frontend/pages/shoes.php
@@ -1,25 +1,27 @@
 <?php
-if (isset($_GET['id'])) {
-    // Display the product with the ID that is set in the URL
-    $product = Database::getInstance()->query("SELECT * FROM products WHERE id = {$_GET['id']}")->results();
-} else {
-    // Display all products
-    $product = Database::getInstance()->query("SELECT * FROM products")->results();
+$products = Database::getInstance()->query("SELECT * FROM products WHERE product_id BETWEEN 3 AND 7")->results();
+if ($products) {
+    foreach ($products as $key => $product) {
+        $products[$key] = get_object_vars($product);
+    }
 }
 ?>
 <!DOCTYPE html>
 <html>
 <body>
     <div class="container" style="margin-top:30px">
-        <h2>Product</h2>
+        <h2>Shoes</h2>
         <ul style="list-style-type: none; display: flex; flex-wrap: wrap;">
+            <?php foreach ($products as $product): ?>
             <li style="margin: 10px; padding: 10px; border: 1px solid #000;">
-                <?php echo "{$product[0]['name']} - {$product[0]['price']} DKK"; ?>
+                <a href="index.php?product_id=<?= $product['product_id']; ?>">
+                    <?php echo "{$product['name']} - {$product['price']} DKK"; ?>
+                </a>
                 <form method="post">
-                    <input type="hidden" name="product_id" value="<?= $product[0]['id']; ?>">
-                    <input type="submit" name="add_to_cart" value="Add to cart">
+                    <input type="hidden" name="product_id" value="<?= $product['product_id']; ?>">
                 </form>
             </li>
+            <?php endforeach; ?>
         </ul>
     </div>
 </body>
