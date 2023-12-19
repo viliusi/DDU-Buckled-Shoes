921a588ec9fb968ccd566fe55b77f096048792ef
Tobias <tobiasflament2@gmail.com>
2023-12-19 10:50:14 +0100

    Cart worknt

--- app/frontend/pages/cart.php
+++ app/frontend/pages/cart.php
@@ -31,41 +31,93 @@ $total_price = 0;
 
 // Display the products in the cart
 if (!empty($_SESSION['cart'])) {
+  // Debugging output
+  echo "<pre>";
+  var_dump($_SESSION['cart']);
+  echo "</pre>";
+
   foreach ($_SESSION['cart'] as $product_id) {
-    $product = Product::getProductById($product_id);
-    
-    // Display product details
-    echo "<h3>" . $product->name . "</h3>";
-    echo "<p>" . $product->description . "</p>";
-    echo "<p>Price: $" . $product->price . "</p>";
-    
-    // Button to remove product from cart
-    echo "<form action='cart.php' method='post'>";
-    echo "<input type='hidden' name='product_id' value='$product_id'>";
-    echo "<input type='submit' name='remove_from_cart' value='Remove from Cart'>";
-    echo "</form>";
+    // Debugging output
+    echo "<p>Product ID: $product_id</p>";
+
+    $product = Product::getProductById((int)$product_id);
+
+    // Check if product exists
+    if ($product === null) {
+      echo "<p>Product with ID $product_id not found.</p>";
+      continue;
+    }
     
     // Add product price to total price
     $total_price += $product->price;
+
+    if (isset($_POST['add_more_product'])) {
+      //then + another product to the total price and add one to quantity
+      $total_price += $product->price;
+      // Assuming you have a way to track quantity per product in your cart
+      // $_SESSION['cart'][$product_id] += 1;
+    }
+
+    if (isset($_POST['remove_product'])) {
+      if (isset($_SESSION['cart'][$product_id]) && $_SESSION['cart'][$product_id] > 0) {
+        $_SESSION['cart'][$product_id] -= 1;
+        $total_price -= $product->price;
+      }
+    }
   }
 
-  echo "<p>Total price: $$total_price</p>";
 
-  $tax_cut = $total_price * 0.2;
-  echo "<p>Tax cut: $$tax_cut</p>";
+// Display the products in the cart
+foreach ($_SESSION['cart'] as $product_id => $quantity) {
+  $product = Product::getProductById($product_id);
 
-  // Button to clear cart
-  echo "<form action='cart.php' method='post'>";
-  echo "<input type='submit' name='clear_cart' value='Clear Cart'>";
-  echo "</form>";
+  // Check if product exists
+  if ($product === null) {
+    echo "<p>Product with ID $product_id not found.</p>";
+    continue;
+  }
 
-  // Button to checkout
-  echo "<form action='checkout.php' method='post'>";
-  echo "<input type='submit' name='checkout' value='Checkout'>";
-  echo "</form>";
-  
-} else {
-  echo "<p>Your cart is empty.</p>";
+  // Display product details
+  echo "<h3>" . $product->name . "</h3>";
+  echo "<p>" . $product->description . "</p>";
+  echo "<p>Price: $" . $product->price . "</p>";
+  echo "<p>Quantity: ". $quantity ."</p>";
 }
+
+      // Button to remove product from cart
+     echo "<form action='cart.php' method='post'>";
+     echo "<input type='hidden' name='product_id' value='$product_id'>";
+     echo "<input type='submit' name='remove_from_cart' value='Remove from Cart'>";
+     echo "</form>";
+
+      // Add more of the same product
+      echo "<form action='cart.php' method='post'>";
+      echo "<input type='hidden' name='product_id' value='$product_id'>";
+      echo "<input type='submit' name='add_more_product' value='+1'>";
+     echo "</form>";
+
+      // Remove one of the same product
+      echo "<form action='cart.php' method='post'>";
+      echo "<input type='hidden' name='product_id' value='$product_id'>";
+      echo "<input type='submit' name='remove_product' value='-1'>";
+      echo "</form>";
+
+      echo "<p>Total price: $$total_price</p>";
+
+      $tax_cut = $total_price * 0.2;
+      echo "<p>Tax cut: $$tax_cut</p>";
+    
+      // Button to clear cart
+      echo "<form action='cart.php' method='post'>";
+      echo "<input type='submit' name='clear_cart' value='Clear Cart'>";
+      echo "</form>";
+    
+      // Button to checkout
+      echo "<form action='checkout.php' method='post'>";
+      echo "<input type='submit' name='checkout' value='Checkout'>";
+      echo "</form>";
+    } else {
+      echo "<p>Your cart is empty.</p>";
+    }
 ?>
 </div>
