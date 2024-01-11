4448850f23b7085b5466d67cf76e71ddc29a96e2
Tobias <tobiasflament2@gmail.com>
2024-01-11 12:53:09 +0100

    Cart done, and order under way

--- app/frontend/pages/checkout.php
+++ app/frontend/pages/checkout.php
@@ -1,24 +1,48 @@
 <div class="container" style="margin-top:30px">
   <h2>Buy</h2>
-<?php
-    //Show the cart items
+  <?php
+
+  
+  // Check if the user is logged in and the cart is not empty
+  if (!isset($_SESSION['user']) || !isset($_SESSION['cart'])) {
+    echo "You must be logged in and have items in your cart to checkout.";
+    exit;
+  }
+  if (isset($_SESSION['user_id']) || isset($_SESSION['cart']))
+    // Show the cart items
     $cart = $_SESSION['cart'];
-    $total_price = 0;
-    foreach ($cart as $product_id) {
-        $product = Product::getProductById($product_id);
-        echo "<h3>" . $product->name . "</h3>";
-        echo "<p>" . $product->description . "</p>";
-        echo "<p>Price: $" . $product->price . "</p>";
-        $total_price += $product->price;
+  $total_price = 0;
+  foreach ($cart as $item) {
+    $product_id = $item['product_id'];
+    $quantity = $item['quantity'];
+    $product = Product::getProductById($product_id);
+    echo "<h3>" . $product->name . "</h3>";
+    echo "<p>" . $product->description . "</p>";
+    echo "<p>Price: $" . $product->price . "</p>";
+    echo "<p>Quantity: " . $quantity . "</p>";
+    $total_price += $product->price * $quantity;
+  }
+  echo "<p>Total price: $$total_price</p>";
+  $tax_cut = $total_price * 0.2;
+  echo "<p>Tax cut: $$tax_cut</p>";
+
+  $user_id = $_SESSION['user_id'];
+  $user = User::getUserById($user_id);
+
+  // Buy button
+  ?>
+  <form action="orders.php" method="post">
+    <input type="hidden" name="user_id" value="<?php echo $user->user_id ?>">
+
+    <?php
+    $products = "";
+    foreach ($cart as $item) {
+      $product_id = $item['product_id'];
+      $quantity = $item['quantity'];
+      $products .= $product_id . "," . $quantity . ";";
     }
-    echo "<p>Total price: $$total_price</p>";
-    $tax_cut = $total_price * 0.2;
-    echo "<p>Tax cut: $$tax_cut</p>";
-
-
-    //Buy button
-    echo "<form action='buy.php' method='post'>";
-    echo "<input type='submit' name='checkout' value='Buy'>";
-    echo "</form>";
-?>
+    ?>
+    <input type="hidden" name="products" value="<?php echo $products ?>">
+    <input type="submit" value="Buy">
+  </form>
 </div>
---
+++ orders.php
@@ -0,0 +1,8 @@
+<?php require_once 'start.php'; ?>
+<?php require_once BACKEND_CLASSES . 'Product.php'; ?>
+<?php require_once FRONTEND_INCLUDE . 'header.php'; ?>
+<?php require_once FRONTEND_INCLUDE . 'navbar.php'; ?>
+<?php require_once FRONTEND_INCLUDE . 'messages.php'; ?>
+<?php require_once FRONTEND_PAGE . 'checkout.php'; ?>
+<?php require_once FRONTEND_INCLUDE . 'footer.php'; ?>
+<?php require_once BACKEND_AUTH . 'add-order.php'; ?>
