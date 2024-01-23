a289f91470cfb5ccff2916610874a6a46f490a3f
Sune <sune171205@gmail.com>
2024-01-23 12:07:03 +0100

    Forside

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -22,7 +22,7 @@
     <div id="slideshow">
       <a id="slideshowLink" href="#">
         <div style="display: flex; align-items: center; justify-content: flex-end; position: relative;">
-          <img id="slideshowImage" src="<?php echo isset($productImages[0]) ? $productImages[0] : ''; ?>" alt="Slideshow Image" style="width: 100%; border-left: 5px solid yellow; border-bottom: 5px solid yellow;">
+          <img id="slideshowImage" src="<?php echo isset($productImages[0]) ? $productImages[0] : ''; ?>" alt="Slideshow Image">
           <div class="shoe-name" style="color: #fff; text-align: right; position: absolute; top: 50%; transform: translateY(-50%); margin-right: 10px;"><?php echo isset($products[0]->product_name) ? addslashes($products[0]->product_name) : ''; ?></div>
         </div>
       </a>
@@ -102,3 +102,53 @@
 <div>
   <h2>Accessories</h2>
   </div>
+
+  <?php
+$products = Database::getInstance()->query("SELECT * FROM products WHERE Category = 'accessory'")->results();
+if ($products) {
+    foreach ($products as $key => $product) {
+        $products[$key] = get_object_vars($product);
+    }
+}
+?>
+<html>
+
+<body>
+    <div class="container" style="margin-top:75px">
+        <h2>Accessories</h2>
+        <ul style="list-style-type: none; display: flex; flex-wrap: wrap;">
+            <?php foreach ($products as $product) : ?>
+                <li style="margin: 10px; padding: 10px;">
+                    <a href="product.php?product_id=<?= $product['product_id']; ?>" style="text-decoration: none; color: inherit;" class="productText">
+                        <div id="productBox" class="outerBorderW product-box" style="position: relative;">
+                            <?php echo "{$product['name']}"; ?>
+                            <form method="post">
+                                <input type="hidden" name="product_id" value="<?= $product['product_id']; ?>">
+                            </form>
+                            <?php
+                            $images = Product::getImagesByProductId($product['product_id']);
+                            if (!empty($images->results())) {
+                                $image = $images->results()[0];
+                            ?>
+                                <img class="product-image" src='<?= $image->image_location ?>' width='200px' height='200px'>
+                            <?php
+                            }
+                            ?>
+                            <div class="pricetag">
+                                <?php $price = Product::getCurrentPrice($product['product_id']) ?>
+                                <?php echo "$" . $price; ?>
+                                    <?php $priceO = Product::getOriginalPrice($product['product_id']) ?>
+                                    <?php echo "<span>$" . $priceO . "</span>"; ?>
+
+                                <?php /* $discount = Product::getDiscount($product['product_id']) */ ?>
+                                <?php /*echo $discount . "🍔";*/ ?>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</body>
+
+</html>
